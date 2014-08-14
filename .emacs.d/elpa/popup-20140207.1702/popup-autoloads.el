0dc1985137fcb117b6184c7807c7bc4779d2b1ed
Cesar A. Quiroz, Ph.D <cesar_quiroz@ieee.org>
2014-08-14 00:46:35 -0700

    Sync ELPA

--- .emacs.d/elpa/popup-20140207.1702/popup-autoloads.el
+++
@@ -1,18 +0,0 @@
-;;; popup-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil nil ("popup-pkg.el" "popup.el") (21319 35842
-;;;;;;  731026 952000))
-
-;;;***
-
-(provide 'popup-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; popup-autoloads.el ends here
