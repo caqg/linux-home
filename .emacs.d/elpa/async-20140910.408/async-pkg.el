d9a804523f862f2aeb04885f0af0bbcbf5cc6d5d
Cesar A. Quiroz, Ph.D <cesar_quiroz@ieee.org>
2014-09-14 00:24:22 -0700

    Sync ELPA

--- .emacs.d/elpa/async-20140910.408/async-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "async" "20140910.408" "Asynchronous processing in Emacs" 'nil)
--- .emacs.d/elpa/relative-line-numbers-20140907.938/relative-line-numbers-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "relative-line-numbers" "20140907.938" "Display relative line numbers on the margin" (quote ((emacs "24"))))
