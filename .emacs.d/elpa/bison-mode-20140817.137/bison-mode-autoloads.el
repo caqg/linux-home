67fab5ad686e457f858d93d868af54f870b3491d
Cesar A. Quiroz, Ph.D <cesar_quiroz@ieee.org>
2014-10-18 22:52:31 -0700

    Sync ELPA

--- .emacs.d/elpa/bison-mode-20140817.137/bison-mode-autoloads.el
+++
@@ -1,33 +0,0 @@
-;;; bison-mode-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads (bison-mode) "bison-mode" "bison-mode.el" (21488
-;;;;;;  62598 981814 207000))
-;;; Generated autoloads from bison-mode.el
-
-(add-to-list 'auto-mode-alist '("\\.y\\'" . bison-mode))
-
-(add-to-list 'auto-mode-alist '("\\.l\\'" . bison-mode))
-
-(autoload 'bison-mode "bison-mode" "\
-Major mode for editing bison/yacc files.
-
-\(fn)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("bison-mode-pkg.el") (21488 62599 441251
-;;;;;;  66000))
-
-;;;***
-
-(provide 'bison-mode-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; bison-mode-autoloads.el ends here
---
+++ .emacs.d/elpa/bison-mode-20141018.1019/bison-mode-pkg.el
@@ -0,0 +1,1 @@
+(define-package "bison-mode" "20141018.1019" "Major mode for editing bison, yacc and lex files." (quote nil))
