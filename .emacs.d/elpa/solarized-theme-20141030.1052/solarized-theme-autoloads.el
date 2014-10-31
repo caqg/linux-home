35941cd5d2f9a6e16a1eaac4532c4a90109a20ea
Cesar A. Quiroz, Ph.D <cesar_quiroz@ieee.org>
2014-10-30 23:55:10 -0700

    init+ELPA for emacs 24.4

--- .emacs.d/elpa/solarized-theme-20141030.1052/solarized-theme-autoloads.el
+++ .emacs.d/elpa/solarized-theme-20141030.1052/solarized-theme-autoloads.el
@@ -1,10 +1,10 @@
 ;;; solarized-theme-autoloads.el --- automatically extracted autoloads
 ;;
 ;;; Code:
-
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "solarized" "solarized.el" (21553 56094 866181
-;;;;;;  114000))
+;;;### (autoloads nil "solarized" "solarized.el" (21587 12507 356189
+;;;;;;  477000))
 ;;; Generated autoloads from solarized.el
 
 (when (and (boundp 'custom-theme-load-path) load-file-name) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
@@ -13,15 +13,13 @@
 
 ;;;### (autoloads nil nil ("solarized-dark-theme.el" "solarized-light-theme.el"
 ;;;;;;  "solarized-theme-pkg.el" "solarized-theme-utils.el" "solarized-theme.el")
-;;;;;;  (21553 56094 953680 503000))
+;;;;;;  (21587 12507 705122 679000))
 
 ;;;***
 
-(provide 'solarized-theme-autoloads)
 ;; Local Variables:
 ;; version-control: never
 ;; no-byte-compile: t
 ;; no-update-autoloads: t
-;; coding: utf-8
 ;; End:
 ;;; solarized-theme-autoloads.el ends here
