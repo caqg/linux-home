a20f21ec3db7f664140b7a05cdd0ddd1c359ea8f
Cesar A. Quiroz, Ph.D <cesar_quiroz@ieee.org>
2015-02-07 19:45:38 -0800

    Sync ELPA

--- .emacs.d/elpa/aes-20150117.408/aes-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "aes" "20150117.408" "Implementation of AES" 'nil :url "https://github.com/gaddhi/aes" :keywords '("data" "tools"))
---
+++ .emacs.d/elpa/aes-20150206.341/aes-pkg.el
@@ -0,0 +1,1 @@
+(define-package "aes" "20150206.341" "Implementation of AES" 'nil :url "https://github.com/Sauermann/emacs-aes" :keywords '("data" "tools"))
