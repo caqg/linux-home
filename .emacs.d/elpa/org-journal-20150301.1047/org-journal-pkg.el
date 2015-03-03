4df69587ae5bf182b5b45a89d670d3033c818eb8
Cesar A. Quiroz, Ph.D <cesar_quiroz@ieee.org>
2015-03-02 22:56:15 -0800

    Sync ELPA

---
+++ .emacs.d/elpa/org-journal-20150301.1047/org-journal-pkg.el
@@ -0,0 +1,1 @@
+(define-package "org-journal" "20150301.1047" "a simple org-mode based journaling mode" 'nil :url "http://github.com/bastibe/org-journal")
--- .emacs.d/elpa/wiki-nav-20140512.702/wiki-nav-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "wiki-nav" "20140512.702" "Simple file navigation using [[WikiStrings]]" (quote ((button-lock "1.0.0") (nav-flash "1.0.0"))))
