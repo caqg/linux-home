805a1c016a6b3eb89f2aeeeb5df7cd928d8c54db
Cesar A. Quiroz, Ph.D <cesar_quiroz@ieee.org>
2015-03-16 20:51:43 -0700

    Sync ELPA

--- .emacs.d/elpa/anti-zenburn-theme-20150315.1559/anti-zenburn-theme-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "anti-zenburn-theme" "20150315.1559" "Low-contrast Zenburn-inverted theme" 'nil :url "https://github.com/m00natic/anti-zenburn-theme")
