211d68b272b5976dfd6051d0392efdb823044d10
Cesar A. Quiroz, Ph.D <cesar_quiroz@ieee.org>
2015-03-29 18:53:17 -0700

    Sync ELPA

--- .emacs.d/elpa/org-jira-20150311.243/org-jira-pkg.el
+++
@@ -1,4 +0,0 @@
-(define-package "org-jira" "20150311.243" "Syncing between Jira and Org-mode." 'nil :url "https://github.com/baohaojun/org-jira")
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
