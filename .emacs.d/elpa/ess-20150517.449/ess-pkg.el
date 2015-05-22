47821614ab6599a6a9300235be0c25bca1239a55
Cesar A. Quiroz, Ph.D <cesar_quiroz@ieee.org>
2015-05-21 17:42:09 -0700

    Sync ELPA

--- .emacs.d/elpa/ess-20150517.449/ess-pkg.el
+++
@@ -1,4 +0,0 @@
-(define-package "ess" "20150517.449" "Emacs Speaks Statistics" 'nil :url "http://ess.r-project.org")
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
--- .emacs.d/elpa/peep-dired-20150505.2039/peep-dired-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "peep-dired" "20150505.2039" "Peep at files in another window from dired buffers" 'nil :keywords '("files" "convenience"))
