2e790a27738321d9b19e5d1fca20deb52611675f
Cesar A. Quiroz, Ph.D <cesar_quiroz@ieee.org>
2015-06-28 00:16:16 -0700

    Sync ELPA

---
+++ .emacs.d/elpa/cmake-mode-20150625.739/cmake-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "cmake-mode" "20150625.739" "major-mode for editing CMake sources" 'nil)
---
+++ .emacs.d/elpa/org-dashboard-20150624.32/org-dashboard-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "org-dashboard" "20150624.32" "Visually summarize progress in org files" '((cl-lib "0.5")) :url "http://github.com/bard/org-dashboard" :keywords '("outlines" "calendar"))
