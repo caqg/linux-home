2d3d8178059fda85fa791ab5ba63b1b7ba159811
Cesar A. Quiroz, Ph.D <cesar_quiroz@ieee.org>
2015-07-06 23:41:18 -0700

    Sync ELPA

--- .emacs.d/elpa/dash-20150701.20/dash-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "dash" "20150701.20" "A modern list library for Emacs" 'nil :keywords '("lists"))
--- .emacs.d/elpa/python-mode-20150628.1214/python-mode-pkg.el
+++
@@ -1,4 +0,0 @@
-(define-package "python-mode" "20150628.1214" "Python major mode" 'nil)
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
