fadc425e1d4c4dc9ed8c961ec13ac88024c0d60e
Cesar A. Quiroz, Ph.D <cesar_quiroz@ieee.org>
2015-07-09 00:58:17 -0700

    Sync ELPA

--- .emacs.d/elpa/org-download-20150414.1005/org-download-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "org-download" "20150414.1005" "Image drag-and-drop for Emacs org-mode" '((async "1.2")) :url "http://orgmode.org" :keywords '("images" "screenshots" "download"))
---
+++ .emacs.d/elpa/org-journal-20150706.2354/org-journal-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "org-journal" "20150706.2354" "a simple org-mode based journaling mode" 'nil :url "http://github.com/bastibe/org-journal")
