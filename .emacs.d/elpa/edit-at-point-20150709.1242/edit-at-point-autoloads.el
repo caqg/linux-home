2369c1ec3ce6305544b80bacc0a71c4608350d20
Cesar A. Quiroz, Ph.D <cesar_quiroz@ieee.org>
2015-07-17 21:20:58 -0700

    Sync ELPA, +mmt

--- .emacs.d/elpa/edit-at-point-20150709.1242/edit-at-point-autoloads.el
+++
@@ -1,16 +0,0 @@
-;;; edit-at-point-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("edit-at-point.el") (21919 25323 941788
-;;;;;;  896000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; edit-at-point-autoloads.el ends here
