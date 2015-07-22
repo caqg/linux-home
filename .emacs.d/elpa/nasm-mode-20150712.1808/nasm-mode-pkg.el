972b349effd1df69a10e3410f8e1f77aed6a2d8c
Cesar A. Quiroz, Ph.D <cesar_quiroz@ieee.org>
2015-07-21 22:57:28 -0700

    Sync ElPA

--- .emacs.d/elpa/nasm-mode-20150712.1808/nasm-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "nasm-mode" "20150712.1808" "NASM x86 assembly major mode" '((emacs "24.3")) :url "https://github.com/skeeto/nasm-mode")
---
+++ .emacs.d/elpa/pandoc-mode-20150720.827/pandoc-mode-autoloads.el
@@ -0,0 +1,33 @@
+;;; pandoc-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "pandoc-mode" "pandoc-mode.el" (21933 53205
+;;;;;;  443522 179000))
+;;; Generated autoloads from pandoc-mode.el
+
+(autoload 'pandoc-mode "pandoc-mode" "\
+Minor mode for interacting with Pandoc.
+
+\(fn &optional ARG)" t nil)
+
+(autoload 'conditionally-turn-on-pandoc "pandoc-mode" "\
+Turn on pandoc-mode if a pandoc settings file exists.
+This is for use in major mode hooks.
+
+\(fn)" nil nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("pandoc-mode-pkg.el" "pandoc-mode-utils.el")
+;;;;;;  (21933 53205 467521 309000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; pandoc-mode-autoloads.el ends here
