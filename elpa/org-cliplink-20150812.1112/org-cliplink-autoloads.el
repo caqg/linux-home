626ae14b32dacfc327694d60325ea0c0b0f5b400
Cesar A. Quiroz, Ph.D <cesar_quiroz@ieee.org>
2015-08-13 21:57:24 -0700

    Sync ELPA

---
+++ elpa/org-cliplink-20150812.1112/org-cliplink-autoloads.el
@@ -0,0 +1,39 @@
+;;; org-cliplink-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "org-cliplink" "org-cliplink.el" (21965 29787
+;;;;;;  21412 880000))
+;;; Generated autoloads from org-cliplink.el
+
+(autoload 'org-cliplink-retrieve-title "org-cliplink" "\
+
+
+\(fn URL TITLE-CALLBACK)" nil nil)
+
+(autoload 'org-cliplink-insert-transformed-title "org-cliplink" "\
+
+
+\(fn URL TRANSFORMER)" nil nil)
+
+(autoload 'org-cliplink-retrieve-title-synchronously "org-cliplink" "\
+
+
+\(fn URL)" nil nil)
+
+(autoload 'org-cliplink "org-cliplink" "\
+Takes a URL from the clipboard and inserts an org-mode link
+with the title of a page found by the URL into the current
+buffer
+
+\(fn)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; org-cliplink-autoloads.el ends here
