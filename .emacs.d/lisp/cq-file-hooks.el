e0ffa5d4a103555b222c224a0a6ed7f15946d9af
Cesar A. Quiroz, Ph.D <cesar_quiroz@ieee.org>
2015-08-18 01:05:04 -0700

    Carefully trim wsp in makefile-mode and derived
    As makefile-mode is a prog-mode, enabling the whitespace trimming on
    save (provided by cq/trim-whitespace) became dangerous. If the
    whitespace-style contained indentation, indentation:tab or
    indentation-space, the trimming may change significant indentation.
    
    This commit has a slightly more general mechanism than treating all the
    makefile-mode derivatives specially. Instead, an alist remembers pairs
    of mode symbols and functions that tailor the whitespace-style
    accordingly to the current major-mode. At this point there is only one
    entry, for makefile-mode.
    
    Now, instead of (eq major-mode makefile-mode), we
    use (derived-mode-p (car constraint)) to catch makefile-mode and all
    modes derived from it (for instance makefile-gmake-mode, which we use
    all the time).

--- .emacs.d/lisp/cq-file-hooks.el
+++ .emacs.d/lisp/cq-file-hooks.el
@@ -8,16 +8,49 @@
 
 (defvar *cq/trim-whitespace-excluded-modes*
   (list 'fundamental-mode 'text-mode 'picture-mode 'markdown-mode)
-  "A list of symbols, mode names, exempted by `cq/trim-whitespace'.")
+  "A list of symbols, mode names, exempted by `cq/trim-whitespace'")
+
+(defvar *cq/trim-whitespace-inherited-constraints*
+  (list (cons 'makefile-mode #'(lambda (wspstyl)
+                                 (mapc
+                                  #'(lambda (excluded-style)
+                                      (setq wspstyl
+                                            (remove excluded-style wspstyl)))
+                                  '(indentation
+                                          indentation:tab
+                                          indentation:space))
+                                 wspstyl)))
+  "An alist of major mode symbols and functions that return a value for
+whitespace-style' to use if the current `major-mode' is derived from the key
+one.")
+
+(defun cq/pick-whitespace-inherited-constraints ()
+  "Return a function that, when applied to the value of `whitespace-style' will
+return a new value for that variable, suitable for the current major-mode"
+  (let ((constraints *cq/trim-whitespace-inherited-constraints*)
+        (result 'identity))
+    (while constraints
+      (let ((constraint (car constraints)))
+        (cond ((derived-mode-p (car constraint))
+               (setq result (cdr constraint))
+               (setq constraints nil))
+              (t
+               (setq constraints (cdr constraints))))))
+    result))
+
+(defun cq/tailor-whitespace-style ()
+  "Return a value for whitespace-style, tailoed to the currrent major mode per
+`*cq/trim-whitespace-inherited-constraints*'"
+  (let ((whitespace-style whitespace-style)
+        (constraint (cq/pick-whitespace-inherited-constraints)))
+    (funcall constraint whitespace-style)))
 
 (defun cq/trim-whitespace ()
-  "On saving, delete trailing whitespace per line, and empty lines at end."
-  (let ((delete-trailing-line t))
-    (unless (memq major-mode *cq/trim-whitespace-excluded-modes*)
-      ;; (delete-trailing-whitespace)
+  "On saving, cleanup whitespace all over the buffer"
+  (unless (memq major-mode *cq/trim-whitespace-excluded-modes*)
+    (let ((whitespace-style (cq/tailor-whitespace-style)))
       (whitespace-cleanup)))
   t)
-
 
 (provide 'cq-file-hooks)
 ;;;; end cq-file-hooks.el
