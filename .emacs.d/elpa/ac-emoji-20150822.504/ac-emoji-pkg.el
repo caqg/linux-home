ca396903f5c29a785ac3942868d6abe12cc1290b
Cesar A. Quiroz, Ph.D <cesar_quiroz@ieee.org>
2015-08-26 11:08:41 -0700

    Sync ELPA

--- .emacs.d/elpa/ac-emoji-20150822.504/ac-emoji-pkg.el
+++
@@ -1,7 +0,0 @@
-(define-package "ac-emoji" "20150822.504" "auto-complete source of Emoji"
-  '((auto-complete "1.5.0")
-    (cl-lib "0.5"))
-  :url "https://github.com/syohex/emacs-ac-emoji")
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
--- .emacs.d/elpa/ess-20150821.820/ess-pkg.el
+++
@@ -1,4 +0,0 @@
-(define-package "ess" "20150821.820" "Emacs Speaks Statistics" 'nil :url "http://ess.r-project.org")
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
