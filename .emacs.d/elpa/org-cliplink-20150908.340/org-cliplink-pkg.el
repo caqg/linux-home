d778b2a3bf9691ffa6ebc15c5cd3195e9e14f824
Cesar A. Quiroz, Ph.D <cesar_quiroz@ieee.org>
2015-09-13 00:58:26 -0700

    Sync ELPA

---
+++ .emacs.d/elpa/org-cliplink-20150908.340/org-cliplink-pkg.el
@@ -0,0 +1,4 @@
+(define-package "org-cliplink" "20150908.340" "insert org-mode links from the clipboard" 'nil :url "http://github.com/rexim/org-cliplink")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
--- .emacs.d/elpa/python-mode-20150831.2336/python-mode-pkg.el
+++
@@ -1,4 +0,0 @@
-(define-package "python-mode" "20150831.2336" "Python major mode" 'nil)
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
