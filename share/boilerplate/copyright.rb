9eb7a92f0dbc8d11b59d54ce0149dc87c5be8882
Cesar A. Quiroz, Ph.D <cesar_quiroz@ieee.org>
2015-09-19 23:32:55 -0700

    Remove filename from end markers
    The boilerplate files should not invite the user to append a filename to
    the end marker (## end, //// end, etc.). If the file populated this way
    is renamed, that final comment is liable to be forgotten and left wrong
    forever.

--- share/boilerplate/copyright.rb
+++ share/boilerplate/copyright.rb
@@ -5,5 +5,4 @@
 ## mailto:cesar.quiroz@gmail.com
 
 
-
 ## end
--- share/boilerplate/copyright.sh
+++ share/boilerplate/copyright.sh
@@ -3,3 +3,5 @@
 ## 3595 Granada Ave Unit 114 / Santa Clara, CA 95051 / USA
 ## All Rights Reserved Worldwide
 ## mailto:cesar.quiroz@gmail.com
+
+# end
