acc51474428104ba9382124a08e6ec17e6076db8
Cesar A. Quiroz, Ph.D <cesar_quiroz@ieee.org>
2015-10-01 01:39:09 -0700

    Sync ELPA

---
+++ .emacs.d/elpa/org-download-20150922.258/org-download-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "org-download" "20150922.258" "Image drag-and-drop for Emacs org-mode" '((async "1.2")) :url "http://orgmode.org" :keywords '("images" "screenshots" "download"))
