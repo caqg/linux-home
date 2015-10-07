9fac13a01d4b74f26751a2f3d71a1b74216c1d17
Cesar A. Quiroz, Ph.D <cesar_quiroz@ieee.org>
2015-10-06 22:35:35 -0700

    Sync ELPA

--- .emacs.d/elpa/pandoc-mode-20150907.313/pandoc-mode-autoloads.el
+++
@@ -1,33 +0,0 @@
-;;; pandoc-mode-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "pandoc-mode" "pandoc-mode.el" (21998 2572
-;;;;;;  317401 401000))
-;;; Generated autoloads from pandoc-mode.el
-
-(autoload 'pandoc-mode "pandoc-mode" "\
-Minor mode for interacting with Pandoc.
-
-\(fn &optional ARG)" t nil)
-
-(autoload 'conditionally-turn-on-pandoc "pandoc-mode" "\
-Turn on pandoc-mode if a pandoc settings file exists.
-This is for use in major mode hooks.
-
-\(fn)" nil nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("pandoc-mode-pkg.el" "pandoc-mode-utils.el")
-;;;;;;  (21998 2572 345400 508000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; pandoc-mode-autoloads.el ends here
