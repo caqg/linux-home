539645878f4f280b922f95228ba611e54a6be0fd
Cesar A. Quiroz, Ph.D <cesar_quiroz@ieee.org>
2015-10-22 21:39:03 -0700

    Sync ELPA and apt-clone for 15.10

---
+++ .emacs.d/elpa/beacon-20151021.1442/beacon-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "beacon" "20151021.1442" "Highlight the cursor whenever the window scrolls" '((seq "1.9")) :url "https://github.com/Malabarba/beacon" :keywords '("convenience"))
---
+++ .emacs.d/elpa/dash-20151021.113/dash-autoloads.el
@@ -0,0 +1,15 @@
+;;; dash-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("dash.el") (22057 33133 827614 252000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; dash-autoloads.el ends here
