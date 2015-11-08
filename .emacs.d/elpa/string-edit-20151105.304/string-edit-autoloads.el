4c54709fd4a193133ad54e2105bfeee85790cf6c
Cesar A. Quiroz, Ph.D <cesar_quiroz@ieee.org>
2015-11-07 18:06:37 -0800

    Sync ELPA

---
+++ .emacs.d/elpa/string-edit-20151105.304/string-edit-autoloads.el
@@ -0,0 +1,22 @@
+;;; string-edit-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "string-edit" "string-edit.el" (22075 41791
+;;;;;;  124041 183000))
+;;; Generated autoloads from string-edit.el
+
+(autoload 'string-edit-at-point "string-edit" "\
+
+
+\(fn)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; string-edit-autoloads.el ends here
