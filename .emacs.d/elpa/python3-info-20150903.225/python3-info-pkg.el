4db47b6e0ae4092eb3d8e7014364ca45bac93201
Cesar A. Quiroz, Ph.D <cesar_quiroz@ieee.org>
2015-11-18 22:14:15 -0800

    Sync ELPA

--- .emacs.d/elpa/python3-info-20150903.225/python3-info-pkg.el
+++
@@ -1,4 +0,0 @@
-(define-package "python3-info" "20150903.225" "No description available." 'nil)
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
---
+++ .emacs.d/elpa/python3-info-20151116.2231/python3-info-pkg.el
@@ -0,0 +1,4 @@
+(define-package "python3-info" "20151116.2231" "Python 3 info manual for Emacs" 'nil)
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
