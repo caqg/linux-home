6bd2a2933bf6fba57137cb9a630740a6f2a84996
Cesar A. Quiroz, Ph.D <cesar_quiroz@ieee.org>
2015-11-30 00:49:14 -0800

    Sync ELPA

--- .emacs.d/elpa/systemtap-mode-20121209.1510/systemtap-mode-autoloads.el
+++
@@ -1,34 +0,0 @@
-;;; systemtap-mode-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads (systemtap-mode) "systemtap-mode" "systemtap-mode.el"
-;;;;;;  (21319 39402 155966 393000))
-;;; Generated autoloads from systemtap-mode.el
-
-(add-to-list 'auto-mode-alist '("\\.stp\\'" . systemtap-mode))
-
-(autoload 'systemtap-mode "systemtap-mode" "\
-Major mode for editing SystemTap scripts.
-
-Key bindings:
-\\{systemtap-mode-map}
-
-\(fn)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("systemtap-mode-pkg.el") (21319 39402
-;;;;;;  248558 41000))
-
-;;;***
-
-(provide 'systemtap-mode-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; systemtap-mode-autoloads.el ends here
---
+++ .emacs.d/elpa/systemtap-mode-20151122.1140/systemtap-mode-autoloads.el
@@ -0,0 +1,27 @@
+;;; systemtap-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "systemtap-mode" "systemtap-mode.el" (22098
+;;;;;;  34331 737998 723000))
+;;; Generated autoloads from systemtap-mode.el
+
+(add-to-list 'auto-mode-alist '("\\.stp\\'" . systemtap-mode))
+
+(autoload 'systemtap-mode "systemtap-mode" "\
+Major mode for editing SystemTap scripts.
+
+Key bindings:
+\\{systemtap-mode-map}
+
+\(fn)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; systemtap-mode-autoloads.el ends here
