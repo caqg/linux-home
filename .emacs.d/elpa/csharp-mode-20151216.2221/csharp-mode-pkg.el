bf9ef76d25ab1c99ec105760790ef46e264a9d7f
Cesar A. Quiroz, Ph.D <cesar_quiroz@ieee.org>
2015-12-18 22:17:18 -0800

    Sync ELPA

---
+++ .emacs.d/elpa/csharp-mode-20151216.2221/csharp-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "csharp-mode" "20151216.2221" "C# mode derived mode" 'nil :url "https://github.com/josteink/csharp-mode" :keywords '("c#" "languages" "oop" "mode"))
---
+++ .emacs.d/elpa/math-symbol-lists-20151215.1043/math-symbol-lists-autoloads.el
@@ -0,0 +1,16 @@
+;;; math-symbol-lists-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil nil ("math-symbol-lists.el") (22130 27913 729455
+;;;;;;  566000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; math-symbol-lists-autoloads.el ends here
