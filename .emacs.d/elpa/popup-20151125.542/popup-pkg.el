23188c4efcef1ba4911ad126067ef97f8db742bc
Cesar A. Quiroz, Ph.D <cesar_quiroz@ieee.org>
2015-12-23 00:05:53 -0800

    Sync ELPA

--- .emacs.d/elpa/popup-20151125.542/popup-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "popup" "20151125.542" "Visual Popup User Interface" '((cl-lib "0.5")) :keywords '("lisp"))
---
+++ .emacs.d/elpa/python-mode-20151222.1312/python-mode-pkg.el
@@ -0,0 +1,4 @@
+(define-package "python-mode" "20151222.1312" "Python major mode" 'nil)
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
