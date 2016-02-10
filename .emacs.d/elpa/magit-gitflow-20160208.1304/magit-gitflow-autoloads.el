26a784fb7c1e6c1205f6b38b224e4f6364e32737
Cesar A. Quiroz, Ph.D <cesar_quiroz@ieee.org>
2016-02-09 23:16:44 -0800

    Sync ELPA

---
+++ .emacs.d/elpa/magit-gitflow-20160208.1304/magit-gitflow-autoloads.el
@@ -0,0 +1,16 @@
+;;; magit-gitflow-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil nil ("magit-gitflow.el") (22201 34413 421942
+;;;;;;  467000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; magit-gitflow-autoloads.el ends here
