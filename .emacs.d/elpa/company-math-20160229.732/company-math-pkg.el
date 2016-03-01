c36f0bb4602a794cbc496325495616d8a888bb35
Cesar A. Quiroz, Ph.D <cesar_quiroz@ieee.org>
2016-03-01 00:49:11 -0800

    Sync ELPA

---
+++ .emacs.d/elpa/company-math-20160229.732/company-math-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "company-math" "20160229.732" "Completion backends for unicode math symbols and latex tags" '((company "0.8.0") (math-symbol-lists "1.0")) :url "https://github.com/vspinu/company-math" :keywords '("unicode" "symbols" "completion"))
