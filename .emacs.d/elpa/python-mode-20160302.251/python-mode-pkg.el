d6d7a19b4291bbd66ae5059c7669f57f739db3de
Cesar A. Quiroz, Ph.D <cesar_quiroz@ieee.org>
2016-03-02 22:52:54 -0800

    Sync ELPA

---
+++ .emacs.d/elpa/python-mode-20160302.251/python-mode-pkg.el
@@ -0,0 +1,4 @@
+(define-package "python-mode" "20160302.251" "Python major mode" 'nil)
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
--- .emacs.d/elpa/srefactor-20151202.2004/srefactor-autoloads.el
+++
@@ -1,30 +0,0 @@
-;;; srefactor-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "srefactor" "srefactor.el" (22184 31963 883970
-;;;;;;  336000))
-;;; Generated autoloads from srefactor.el
-
-(autoload 'srefactor-refactor-at-point "srefactor" "\
-Offer contextual menu with actions based on current tag in scope.
-
-Each menu item added returns a token for what type of refactoring
-to perform.
-
-\(fn)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("srefactor-lisp.el" "srefactor-pkg.el"
-;;;;;;  "srefactor-ui.el") (22184 31963 899969 853000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; srefactor-autoloads.el ends here
