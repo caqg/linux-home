ed9ddd01e645a6713c3cce07876e1c27887909bb
César A. Quiroz, Ph.D <cesar_quiroz@ieee.org>
2016-04-03 16:31:03 -0700

    Sync ELPA

---
+++ .emacs.d/elpa/beacon-20160328.1716/beacon-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "beacon" "20160328.1716" "Highlight the cursor whenever the window scrolls" '((seq "1.11")) :url "https://github.com/Malabarba/beacon" :keywords '("convenience"))
