cda1b223f69f0e5d2b61041f63078ff213892c47
César A. Quiroz, Ph.D <cesar_quiroz@ieee.org>
2016-04-15 20:18:36 -0700

    Sync ELPA

--- .emacs.d/elpa/gscholar-bibtex-20160317.1755/gscholar-bibtex-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "gscholar-bibtex" "20160317.1755" "Retrieve BibTeX from Google Scholar and other online sources(ACM, IEEE, DBLP)" 'nil :keywords '("extensions"))
--- .emacs.d/elpa/string-edit-20151213.930/string-edit-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "string-edit" "20151213.930" "Avoid escape nightmares by editing string in separate buffer" '((dash "1.2.0")))
