cc6d8445aa8b65b448a1ec541ac6bc2435d0e26e
César A. Quiroz, Ph.D <cesar_quiroz@ieee.org>
2016-04-21 11:03:34 -0700

    Sync ELPA

---
+++ .emacs.d/elpa/solarized-theme-20160419.827/solarized-theme-autoloads.el
@@ -0,0 +1,25 @@
+;;; solarized-theme-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "solarized" "solarized.el" (22294 61335 91397
+;;;;;;  981000))
+;;; Generated autoloads from solarized.el
+
+(when (and (boundp 'custom-theme-load-path) load-file-name) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
+
+;;;***
+
+;;;### (autoloads nil nil ("solarized-dark-theme.el" "solarized-light-theme.el"
+;;;;;;  "solarized-theme-pkg.el" "solarized-theme-utils.el" "solarized-theme.el")
+;;;;;;  (22294 61335 111383 646000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; solarized-theme-autoloads.el ends here
