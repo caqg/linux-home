dfeed83c60468ecf039fb9e4a92822b5c3a2be22
César A. Quiroz, Ph.D <cesar_quiroz@ieee.org>
2016-05-09 22:16:23 -0700

    Sync ELPA

--- .emacs.d/elpa/ruby-test-mode-20151127.18/ruby-test-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "ruby-test-mode" "20151127.18" "Minor mode for Behaviour and Test Driven" '((ruby-mode "1.0")) :keywords '("ruby" "unit" "test" "rspec"))
