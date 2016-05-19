9f0334f30096615bd6c8507ace7bac0ee589fc53
César A. Quiroz, Ph.D <cesar_quiroz@ieee.org>
2016-05-19 02:38:20 -0700

    Sync ELPA

--- .emacs.d/elpa/xcscope-20160201.1926/xcscope-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "xcscope" "20160201.1926" "cscope interface for (X)Emacs" 'nil :url "https://github.com/dkogan/xcscope.el" :keywords '("languages" "c"))
