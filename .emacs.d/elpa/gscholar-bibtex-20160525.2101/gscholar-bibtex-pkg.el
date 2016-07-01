e9e988b275ef5719e2d85047b33745e8f01c29bc
César A. Quiroz, Ph.D <cesar_quiroz@ieee.org>
2016-07-01 00:47:51 -0700

    Sync ELPA

--- .emacs.d/elpa/gscholar-bibtex-20160525.2101/gscholar-bibtex-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "gscholar-bibtex" "20160525.2101" "Retrieve BibTeX from Google Scholar and other online sources(ACM, IEEE, DBLP)" 'nil :keywords '("extensions"))
