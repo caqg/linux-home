65632eb1009583f2cb85ed59496fb368b3a21762
César A. Quiroz, Ph.D <cesar_quiroz@ieee.org>
2016-07-24 01:01:03 -0700

    Sync ELPA

--- .emacs.d/elpa/yasnippet-20160723.510/yasnippet-pkg.el
+++ .emacs.d/elpa/yasnippet-20160723.510/yasnippet-pkg.el
@@ -1,4 +1,4 @@
-(define-package "yasnippet" "20160718.1846" "Yet another snippet extension for Emacs."
+(define-package "yasnippet" "20160723.510" "Yet another snippet extension for Emacs."
   '((cl-lib "0.5"))
   :url "http://github.com/capitaomorte/yasnippet" :keywords
   '("convenience" "emulation"))
