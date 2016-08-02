104e84347bd08e12b56bd62cc0ad8620b698fb83
César A. Quiroz, Ph.D <cesar_quiroz@ieee.org>
2016-08-01 21:32:22 -0700

    sync ELPA

---
+++ .emacs.d/elpa/julia-mode-20160801.1517/julia-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "julia-mode" "20160801.1517" "Major mode for editing Julia source code" 'nil :url "https://github.com/JuliaLang/julia" :keywords '("languages"))
