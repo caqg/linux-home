fec79289f0975703fca89a3c7e7c17547330efaf
César A. Quiroz, Ph.D <cesar_quiroz@ieee.org>
2016-08-10 23:40:18 -0700

    Sync ELPA

--- .emacs.d/elpa/codesearch-20160111.655/codesearch-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "codesearch" "20160111.655" "Easy access to the codesearch tools" '((dash "2.8.0")) :url "https://github.com/abingham/codesearch.el" :keywords '("tools" "development" "search"))
