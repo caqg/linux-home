a30f44d91fb55ec217d4b01168a88d7d62bc4f0b
César A. Quiroz, Ph.D <cesar_quiroz@ieee.org>
2016-09-01 00:26:53 -0700

    Sync ELPA

---
+++ .emacs.d/elpa/anything-20160822.1852/anything-pkg.el
@@ -0,0 +1,5 @@
+(define-package "anything" "20160822.1852" "open anything / QuickSilver-like candidate-selection framework" 'nil :url "http://repo.or.cz/w/anything-config.git" :keywords
+  '("files" "frames" "help" "matching" "outlines"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
