7c45ca684227667a86843ff13d0df981e013fec1
César A. Quiroz, Ph.D <cesar_quiroz@ieee.org>
2016-10-13 20:20:18 -0700

    Sync ELPA

---
+++ .emacs.d/elpa/gscholar-bibtex-20161006.1944/gscholar-bibtex-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "gscholar-bibtex" "20161006.1944" "Retrieve BibTeX from Google Scholar and other online sources(ACM, IEEE, DBLP)" 'nil :keywords '("extensions"))
--- .emacs.d/elpa/python-mode-20161004.1054/python-mode-pkg.el
+++
@@ -1,4 +0,0 @@
-(define-package "python-mode" "20161004.1054" "Python major mode" 'nil)
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
