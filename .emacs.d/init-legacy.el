64626067517d0c5c7d76d93f1d3b3e881332bfb7
César A. Quiroz <cesar_quiroz@ieee.org>
2019-08-01 22:05:05 -0700

    Activate FCI-MODE for cmake buffers

--- .emacs.d/init-legacy.el
+++ .emacs.d/init-legacy.el
@@ -81,6 +81,7 @@
 
 (autoload 'cmake-font-lock-activate "cmake-font-lock" nil t)
 (add-hook 'cmake-mode-hook 'cmake-font-lock-activate)
+(add-hook 'cmake-mode-hook #'(lambda () (fci-mode 1))
 
 (add-hook 'c++-mode-hook
           #'(lambda ()
