285b75ad5672c1e2c911861d8e0b9e606739a29e
César A. Quiroz <cesar_quiroz@ieee.org>
2019-08-01 22:18:27 -0700

    Make frames fit at new size (14)

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -227,7 +227,7 @@
  '(cursor-color "#cccccc")
  '(custom-safe-themes
    '("cb8d13429234ff2a8700da4db9bdf6b952c1b54b906a1aad2d0d98317c5b0224" "889a93331bc657c0f05a04b8665b78b3c94a12ca76771342cee27d6605abcd0e" "a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" "4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" "8db4b03b9ae654d4a57804286eb3e332725c84d7cdab38463cb6b97d5762ad26" "c69211d8567a0c5fa14b81c4cd08c4a458db7904c10d95f75d6ecd1b8baf19bd" "a53714de04cd4fdb92ed711ae479f6a1d7d5f093880bfd161467c3f589725453" "39dd7106e6387e0c45dfce8ed44351078f6acd29a345d8b22e7b8e54ac25bac4" "cab317d0125d7aab145bc7ee03a1e16804d5abdfa2aa8738198ac30dc5f7b569" "0c311fb22e6197daba9123f43da98f273d2bfaeeaeb653007ad1ee77f0003037" "e16a771a13a202ee6e276d06098bc77f008b73bbac4d526f160faa2d76c1dd0e" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default))
- '(default-frame-alist '((width . 115) (height . 48)))
+ '(default-frame-alist '((width . 104) (height . 48)))
  '(default-input-method "latin-postfix")
  '(delete-old-versions t)
  '(delete-selection-mode t)
